d167d146f8e5d71a569a87ab9d77532dc1906651
rohitkamat <rohit_kamat15@yahoo.in>
2014-08-27 01:05:06 +0530

    my commit

---
+++ geek.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int *func (int *p,int n);
+int main()
+{
+/*code to show a function returning pointer*/
+int arr[10]={1,2,3,4,5,6,7,8,9,10},n,*ptr;
+n=5;
+ptr=func(arr,n);
+printf("arr=%p ptr=%p,*ptr=%d\n",arr,ptr,*ptr);
+return 0;
+}
+
+int *func(int *p,int n)
+{
+p=p+n;
+return p;
+}
