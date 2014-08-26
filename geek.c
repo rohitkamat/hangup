90844813e2f577ca88f11a2e2e4ebad5d0f94053
rohitkamat <rohit_kamat15@yahoo.in>
2014-08-27 01:36:17 +0530

    lastes geek

--- geek.c
+++ geek.c
@@ -7,7 +7,7 @@ int arr[10]={1,2,3,4,5,6,7,8,9,10},n,*ptr;
 n=5;
 ptr=func(arr,n);
 printf("arr=%p ptr=%p,*ptr=%d\n",arr,ptr,*ptr);
-printf("Geek  point here\n");
+printf("Geek  point is at github\n");
 return 0;
 }
 
